1dfe9d55f468e5829efb01c2bee40bd3550f20b6
Mohamed Shaaban <117195948+sh3boly@users.noreply.github.com>
2026-03-11 23:56:23 +0200

    Added perspective projection and `.obj` model loading

--- CPU Rasterizer/main.cpp
+++ CPU Rasterizer/main.cpp
@@ -1,15 +1,20 @@
 #include <vector>
 #include <cstdio>
 #include <cstdlib>
-#include <fstream>
 #include "My_Math.h"
 #include "PolygonHelper.h"
 #include <chrono>
 #include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 struct Vertex
 {
     Vec3 coordinates;
     Vec3 color;
+    Vec3 normal;
 };
 struct Rgb
 {
@@ -27,23 +32,124 @@ inline float edgeFunction(const Vec2 &a, const Vec2 &b, const Vec2 &c)
 {
     return crossProduct(b - a, c - a);
 }
-#include <iostream>
 const uint32_t w = 512;
 const uint32_t h = 512;
 Rgb *framebuffer = new Rgb[w * h];
 float *zbuffer = new float[w * h];
+float minX = INFINITY, maxX = -INFINITY;
+float minY = INFINITY, maxY = -INFINITY;
+float minZ = INFINITY, maxZ = -INFINITY;
+float size;
+std::vector<Triangle> loadObj(std::string filename) {
+    std::vector<Vec3> t_positions;
+    std::vector<Vec3> temp_positions;
+    std::vector<Vec2> temp_uvs;
+    std::vector<Vec3> temp_normals;
+
+    std::vector<Triangle> mesh;
+
+    std::ifstream file(filename);
+    std::string line;
+    while(std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string prefix;
+        ss >> prefix;
+
+        if (prefix == "v") {
+            Vec3 pos;
+            ss >> pos.x >> pos.y >> pos.z;
+            minX = std::min(pos.x, minX);
+            minY = std::min(pos.y, minY);
+            minZ = std::min(pos.z, minZ);
+            maxX = std::max(pos.x, maxX);
+            maxY = std::max(pos.y, maxY);
+            maxZ = std::max(pos.z, maxZ);
+
+
+            temp_positions.push_back(pos);
+        }
+        else if (prefix == "vt") {
+            Vec2 uv;
+            ss >> uv.x >> uv.y;
+            temp_uvs.push_back(uv);
+        } 
+        else if (prefix == "vn") {
+            Vec3 normal;
+            ss >> normal.x >> normal.y >> normal.z;
+            temp_normals.push_back(normal);
+        } 
+        else if (prefix == "f") {
+            // Here you parse the "v/vt/vn v/vt/vn v/vt/vn" string
+            // Remember to subtract 1 from the indices!
+            // Assemble 3 Vertices, create a Triangle, and add it to 'mesh'
+            int v[3], t[3], n[3];
+            for (int i = 0; i < 3; i++) {
+                std::string vertex_chunk;
+                ss >> vertex_chunk;
+
+                std::stringstream chunk_ss(vertex_chunk);
+                std::string v_str, t_str, n_str;
+
+                std::getline(chunk_ss, v_str, '/');
+                std::getline(chunk_ss, t_str, '/');
+                std::getline(chunk_ss, n_str, '/');
+
+                // Convert strings to ints (subtract 1 for 0-based indexing)
+                // Position is always guaranteed to be there
+                v[i] = std::stoi(v_str) - 1;
+
+                // Only convert UVs and Normals if the string isn't empty
+                t[i] = t_str.empty() ? -1 : std::stoi(t_str) - 1;
+                n[i] = n_str.empty() ? -1 : std::stoi(n_str) - 1;
+            }
+            
+            Vertex v0, v1, v2;
+            v0.coordinates = temp_positions[v[0]];
+            v1.coordinates = temp_positions[v[1]];
+            v2.coordinates = temp_positions[v[2]];
+            if (n[0] != -1) {
+                v0.normal = temp_normals[n[0]];
+            }
+            if (n[1] != -1) {
+                v1.normal = temp_normals[n[1]];
+            }
+            if (n[2] != -1) {
+                v2.normal = temp_normals[n[2]];
+            }
+            mesh.push_back({v0, v1, v2});
+            
+        }
+        else {
+            continue;
+        }
+    }
+    size = std::max(maxX - minX, std::max(maxY - minY, maxZ - minY));
+    return mesh;
+}
+
 void rasterizeTriangle(Triangle &t1)
 {
     Vertex &v0 = t1.vertices[0];
     Vertex &v1 = t1.vertices[1];
     Vertex &v2 = t1.vertices[2];
+    float gray0 = (v0.coordinates.z - minZ) / (maxZ - minZ);
+    float gray1 = (v1.coordinates.z - minZ) / (maxZ - minZ);
+    float gray2 = (v2.coordinates.z - minZ) / (maxZ - minZ);
 
+    v0.color.x = gray0;
+    v0.color.y = gray0;
+    v0.color.z = gray0;
+
+    v1.color.x = gray1;
+    v1.color.y = gray1;
+    v1.color.z = gray1;
+
+    v2.color.x = gray2;
+    v2.color.y = gray2;
+    v2.color.z = gray2;
     float area = edgeFunction((Vec2)v0.coordinates, (Vec2)v1.coordinates, (Vec2)v2.coordinates);
     if (area < 0)
-    {
-        std::swap(v1, v2);
-        area = -area;
-    }
+        return;
 
     // calculate bounding boxes
     std::vector<uint32_t> bbmin = {w, h};
@@ -53,7 +159,10 @@ void rasterizeTriangle(Triangle &t1)
 
     bbmax[0] = std::max(v0.coordinates.x, std::max(v1.coordinates.x, v2.coordinates.x));
     bbmax[1] = std::max(v0.coordinates.y, std::max(v1.coordinates.y, v2.coordinates.y));
-
+    bbmax[0] = std::min(w - 1, bbmax[0]);
+    bbmax[1] = std::min(h - 1, bbmax[1]);
+    bbmin[0] = std::min(w - 1, bbmin[0]);
+    bbmin[1] = std::min(h - 1, bbmin[1]);
     Vec2 p = {bbmin[0] + 0.5f, bbmin[1] + 0.5f};
 
     // Barycentric Coordinates
@@ -91,7 +200,6 @@ void rasterizeTriangle(Triangle &t1)
                 float normalizedW1 = std::max(0.f, w1 * divArea);
                 float normalizedW2 = std::max(0.f, w2 * divArea);
                 float z = normalizedW0 * v0.coordinates.z + normalizedW1 * v1.coordinates.z + normalizedW2 * v2.coordinates.z;
-                z = -z;
                 if (z >= zbuffer[j * w + i])
                     continue;
                 Vec3 &c0 = v0.color;
@@ -155,39 +263,41 @@ void outputPPM()
 
     ofs.close();
 }
-void project(Triangle &tri)
+void project(Triangle &tri, const Mat4 &mvp)
 {
+    for (int i = 0; i < 3; i++) {
+        tri.vertices[i].coordinates.x /= size;
+        tri.vertices[i].coordinates.y /= size;
+        tri.vertices[i].coordinates.z /= size;
+
+        tri.vertices[i].coordinates = mvp * tri.vertices[i].coordinates;
+
+
+        tri.vertices[i].coordinates.x = (tri.vertices[i].coordinates.x + 1) * (w / 2.0f);
+        tri.vertices[i].coordinates.y = (tri.vertices[i].coordinates.y + 1) * (h / 2.0f);
+    }
+
 }
 void testTriangulate()
 {
     memset(framebuffer, 0x0, w * h * sizeof(Rgb));
     memset(zbuffer, 1.0, w * h * sizeof(float));
-    std::vector<Vec2> vertices(5);
-    // We want to something like projecting in the Z-axis so we basically divide by x
-    // we'll asume for now that we are going to have the the thing give
-    vertices[4] = Vec3(200, 100, 50);
-    vertices[3] = Vec3(0, 0, 50);
-    vertices[2] = Vec3(50, 70, 70);
-    vertices[1] = Vec3(50, 130, 20);
-    vertices[0] = Vec3(0, 200, 90);
-    PolygonHelper h;
-    std::string errorMessage;
-    std::vector<int> triangles = h.triangulate(vertices, errorMessage);
-    std::vector<Triangle> tris(3);
-    for (int i = 0; i < 3; i++)
+    std::vector<Triangle> mesh = loadObj("Untitled.obj");
+    
+
+    Mat4 model = makeTranslationMatrix(Vec3(0.0f, 0.0f, -1.0f));
+    Mat4 view = Mat4::identity();
+    Mat4 projectionMatrix = makePerspectiveMatrix(90, (float)w/h, 0.1f, 100.0f);
+
+    Mat4 mvp = view * model;
+    mvp = projectionMatrix * mvp;
+
+    for (auto& tri : mesh)
     {
-        for (int j = 0; j < 3; j++)
-        {
-            tris[i].vertices[j].coordinates = (Vec3)(vertices[triangles[i * 3 + j]]);
-            tris[i].vertices[j].color = Vec3(static_cast<float>(rand()) / RAND_MAX, static_cast<float>(rand()) / RAND_MAX,
-                                             static_cast<float>(rand()) / RAND_MAX);
-        }
+        project(tri, mvp);
     }
-    for (auto tri : tris)
-    {
-        project(tri);
-    }
-    for (auto tri : tris)
+
+    for (auto& tri : mesh)
     {
         rasterizeTriangle(tri);
     }
@@ -197,20 +307,21 @@ void testTriangulate()
 
 int main()
 {
-    std::chrono::duration<double, std::milli> min_duration = std::chrono::duration<double, std::milli>::max();
-    for (int i = 0; i < 99; i++)
-    {
-        auto start = std::chrono::high_resolution_clock::now();
+    //std::chrono::duration<double, std::milli> min_duration = std::chrono::duration<double, std::milli>::max();
+    //for (int i = 0; i < 99; i++)
+    //{ 
+    //    auto start = std::chrono::high_resolution_clock::now();
 
-        testTriangulate();
+    //    testTriangulate();
 
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> current_duration = end - start;
+    //    auto end = std::chrono::high_resolution_clock::now();
+    //    std::chrono::duration<double, std::milli> current_duration = end - start;
 
-        // Update min_duration if the current run was faster
-        min_duration = std::min(min_duration, current_duration);
-    }
-    std::cout << "Fastest execution time: " << min_duration.count() << " ms\n";
-    delete[] framebuffer;
-    delete[] zbuffer;
+    //    // Update min_duration if the current run was faster
+    //    min_duration = std::min(min_duration, current_duration);
+    //}
+    //std::cout << "Fastest execution time: " << min_duration.count() << " ms\n";
+    //delete[] framebuffer;
+    //delete[] zbuffer;
+    testTriangulate();
 }

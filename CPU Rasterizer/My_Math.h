3ebac4f6504e5a40e21375bc30dcdc218d0c08e6
Mohamed Shaaban <117195948+sh3boly@users.noreply.github.com>
2026-03-11 23:55:59 +0200

    Added some matrix helpers for transformations

--- CPU Rasterizer/My_Math.h
+++ CPU Rasterizer/My_Math.h
@@ -162,27 +162,127 @@ public:
 		}
 		return t;
 	}
-	Vec4 operator*(const Vec4& v) const {
-		Vec4 t;
-		t.x = this->mat[0 * 4 + 0] * v.x
-		+ this->mat[0 * 4 + 1] * v.y
-		+ this->mat[0 * 4 + 2] * v.z
-		+ this->mat[0 * 4 + 3] * v.w;
-		t.y = this->mat[1 * 4 + 0] * v.x
-		+this->mat[1 * 4 + 1] * v.y
-		+this->mat[1 * 4 + 2] * v.z
-		+this->mat[1 * 4 + 3] * v.w;
-		t.z = this->mat[2 * 4 + 0] * v.x
-		+this->mat[2 * 4 + 1] * v.y
-		+this->mat[2 * 4 + 2] * v.z
-		+this->mat[2 * 4 + 3] * v.w;
-		t.w = this->mat[3 * 4 + 0] * v.x
-		+this->mat[3 * 4 + 1] * v.y
-		+this->mat[3 * 4 + 2] * v.z
-		+this->mat[3 * 4 + 3] * v.w;
+
+	static Mat4 identity() {
+		Mat4 mat;
+		mat[0] = 1.0f;
+		mat[5] = 1.0f;
+		mat[10] = 1.0f;
+		mat[15] = 1.0f;
+
+		return mat;
+	}
+
+	Vec3 operator*(const Vec3& v) const {
+		Vec3 t;
+		t.x = mat[0] * v.x + mat[4] * v.y + mat[8]  * v.z + mat[12] * 1.0f;
+		t.y = mat[1] * v.x + mat[5] * v.y + mat[9]  * v.z + mat[13] * 1.0f;
+		t.z = mat[2] * v.x + mat[6] * v.y + mat[10] * v.z + mat[14] * 1.0f;
+		float w = mat[3] * v.x + mat[7] * v.y + mat[11] * v.z + mat[15] * 1.0f;
+        
+		if (w != 1.0f && w != 0.0f) {
+			t.x /= w;
+			t.y /= w;
+			t.z /= w;
+		}
 		return t;
 	}
+
+	Vec4 operator*(const Vec4& v) const {
+		Vec4 t;
+		t.x = mat[0] * v.x + mat[4] * v.y + mat[8]  * v.z + mat[12] * v.w;
+		t.y = mat[1] * v.x + mat[5] * v.y + mat[9]  * v.z + mat[13] * v.w;
+		t.z = mat[2] * v.x + mat[6] * v.y + mat[10] * v.z + mat[14] * v.w;
+		t.w = mat[3] * v.x + mat[7] * v.y + mat[11] * v.z + mat[15] * v.w;
+		return t;
+	}
+	float& operator[](int index) {
+		if (index < 0 || index >= 16) {
+		    throw std::out_of_range("Index out of bounds");
+		}
+		return mat[index];
+	}
+
+	const float& operator[](int index) const {
+		if (index < 0 || index >= 16) {
+		    throw std::out_of_range("Index out of bounds");
+		}
+		return mat[index];
+	}
 };
+static Mat4 makeTranslationMatrix(Vec3 offset) {
+	Mat4 mat = Mat4::identity();
+	
+
+	mat[12] = offset.x;
+	mat[13] = offset.y;
+	mat[14] = offset.z;
+	return mat;
+}
+static Mat4 makeZRotationMatrix(float angle) {
+	angle = angle * 3.14159265359f / 180.0f;
+	
+	float sine = std::sin(angle);
+	float cosine = std::cos(angle);
+	Mat4 mat = Mat4::identity();
+	mat[0] = cosine; 
+	mat[1] = sine;
+	mat[4] = -sine;
+	mat[5] = cosine;
+	return mat;
+}
+
+static Mat4 makeYRotationMatrix(float angle) {
+	angle = angle * 3.14159265359f / 180.0f;
+
+	float sine = std::sin(angle);
+	float cosine = std::cos(angle);
+	Mat4 mat = Mat4::identity();
+	mat[0] = cosine; 
+	mat[2] = -sine;
+	mat[5] = 1.0f;
+	mat[8] = sine;
+	mat[10] = cosine;
+	mat[15] = 1.0f;
+	return mat;
+}
+
+static Mat4 makeXRotationMatrix(float angle) {
+	angle = angle * 3.14159265359f / 180.0f;
+
+	float sine = std::sin(angle);
+	float cosine = std::cos(angle);
+	Mat4 mat = Mat4::identity();
+	mat[5] = cosine;
+	mat[6] = sine;
+	mat[9] = -sine;
+	mat[10] = cosine;
+	return mat;
+}
+
+static Mat4 makeScalingMatrix(Vec3 scale) {
+	Mat4 mat = Mat4::identity();
+	mat[0] = scale.x;
+	mat[5] = scale.y;
+	mat[10] = scale.z; 
+
+	return mat;
+}
+
+static Mat4 makePerspectiveMatrix(const float &fovDegrees, const float &aspectRatio, const float &near, const float &far) {
+	Mat4 mat;
+	float fovRad = fovDegrees * 3.14159265359f / 180.0f;
+	float tanHalfFov = std::tan(fovRad / 2.0f);
+
+	mat[0] = (1 / (tanHalfFov * aspectRatio));
+	mat[5] = (1 / tanHalfFov);
+	mat[10] = (-far - near) / (near - far);
+	mat[14] = (2.0f * far * near) / (near - far);
+	mat[11] = -1.0f;
+
+	return mat;
+}
+
 std::ostream& operator<<(std::ostream& out, const Mat4& mat) {
 
 	for (int r = 0; r < 4; r++) {
